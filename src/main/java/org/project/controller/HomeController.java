13555b2eeede3f8c2de25cf55174642b85cfc8a6
elkhailihamza <elkhailihamza331@gmail.com>
2024-10-23 09:37:01 +0100

    made some changes

--- src/main/java/org/project/controller/HomeController.java
+++ src/main/java/org/project/controller/HomeController.java
@@ -1,4 +1,17 @@
 package org.project.controller;
 
-public class HomeController {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+
+public class HomeController implements Controller {
+    @Override
+    public ModelAndView handleRequest(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response) {
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("message", "Welcome to the Home Page!");
+        return modelAndView;
+    }
 }
--- src/main/java/org/project/controller/UserController.java
+++ src/main/java/org/project/controller/UserController.java
@@ -1,4 +1,16 @@
 package org.project.controller;
 
-public class UserController {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class UserController implements Controller {
+    @Override
+    public ModelAndView handleRequest(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response) {
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("message", "input db message here");
+        return modelAndView;
+    }
 }
